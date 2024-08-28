f111b798a2ef5d95034148cce6a73022c0d647ce
Azura <148488760+Ev4ness@users.noreply.github.com>
2024-08-28 21:32:50 +0700

    Update config.py

--- pyromod/config.py
+++ pyromod/config.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 config = SimpleNamespace(
     timeout_handler=None,
     stopped_handler=None,
-    throw_exceptions=True,
+    throw_exceptions=False,
     unallowed_click_alert=True,
     unallowed_click_alert_text=("[pyromod] You're not expected to click this button."),
 )
